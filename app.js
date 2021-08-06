4b66a9d321c607c6a235ee7b8c380e807914dcab
kivey7812 <kirsten.ivey@gmail.com>
2021-08-06 16:27:47 -0700

    Add gitignore, procfile, engine to json file, and update ports

--- app.js
+++ app.js
@@ -81,9 +81,10 @@ app.get("/post/:postId", (req, res)=>{
   });
 });
 
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function(){
+  console.log("server started successfully");
 });
